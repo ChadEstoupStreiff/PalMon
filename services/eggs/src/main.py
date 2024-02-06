9c36ecca6e6beabc49022d73aaeecee8f51255bf
Elouan Lahellec <elouan.lahellec@gmail.com>
2024-02-06 15:59:59 +0100

    fix endpoint_egg_create

--- services/eggs/src/main.py
+++ services/eggs/src/main.py
@@ -18,17 +18,21 @@ app.add_middleware(
 
 @app.get("/egg", tags=["egg"])
 async def endpoint_egg_get(owner) -> None:
-    return DB().get().query(Egg).filter_by(owner=owner).all()
+    result = DB().get().query(Egg).filter_by(owner=owner).first()
+    if result is not None:
+        return result.amount
+    return 0
 
 @app.post("/egg", tags=["egg"])
 async def endpoint_egg_create(egg_owner) -> None:
-    palmon_count = DB().get().query(Egg).filter_by(owner=egg_owner).all()
-    if not palmon_count:
-        palmon_count.append(1)
-    egg = Egg(
-        owner=egg_owner, 
-        amount = palmon_count[0]
-    )
     commit = DB().get()
+    egg = commit.query(Egg).filter_by(owner=egg_owner).first()
+    if egg is not None:
+        egg.amount += 1
+    else:
+        egg = Egg(
+            owner = egg_owner, 
+            amount = 1
+        )
     commit.add(egg)
     commit.commit()
